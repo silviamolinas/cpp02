e955d6a10cc108cd6a48295d0699c0994fe9388b
Silvia Molinas <sil.kibil@gmail.com>
2026-09-15 15:32:00 -0300

    wip: ex01

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,17 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+	private:
+		int	_fixedPoint;
+		const static int _bitsFractional = 8;
+	public:
+		Fixed();
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed();
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+};
+#endif
